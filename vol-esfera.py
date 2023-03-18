93f1ed5b0da7025cd3fb07d579c3131cc5236bf4
Thayná Lopes <75785465+thaynarlt@users.noreply.github.com>
2023-03-18 08:47:49 -0300

    Add files via upload
    Volume da Esfera em Python

---
+++ vol-esfera.py
@@ -0,0 +1,9 @@
+# 4/3 * π * raio3
+
+raio = float(input("Digite o valor do raio da esfera:"))
+pi = 3.14
+raio3 = raio * raio * raio
+
+vol = (4 * pi * raio3)/3
+
+print("O volume da esfera é:{:.2f}". format(vol))
